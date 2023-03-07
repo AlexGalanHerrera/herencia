b7e20fec119af52ef649e88aebf929d541f12be0
matinal <alexgalanherrera5@gmail.com>
2023-03-07 14:40:43 +0100

    hola

---
+++ src/Ejercicio1/Helper.java
@@ -0,0 +1,21 @@
+package Ejercicio1;
+
+    public class Helper {
+        public static String getTipoPension(Pension pension) {
+            switch (pension){
+                case PENSION_COMPLETA:
+                    return  "pensión completa";
+
+                case MEDIA_PENSION:
+                    return  "medio pensión";
+
+                case SIN_PENSION:
+                    return "sin pensión";
+
+                default:
+                    break;
+            }
+            return "";
+        }
+    }
+
